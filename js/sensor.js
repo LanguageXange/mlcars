3abd3bfabd600705b28ac0b1d5ad65ab8579af96
Cindy Lin <hlin0017@seattlecentral.edu>
2023-12-27 14:36:57 -0800

    update sensor to detect road borders and organize files to js folder

---
+++ js/sensor.js
@@ -0,0 +1,92 @@
+class Sensor {
+  constructor(car) {
+    this.car = car;
+    this.rayCount = 5; // ray casting
+    this.rayLength = 120;
+    this.raySpread = Math.PI / 4; // 45 degrees
+
+    this.rays = []; // [[start,end]] length = rayCount
+
+    // detecting how far the road border is
+    this.readings = [];
+  }
+
+  update(roadBorders) {
+    this.#castRays();
+
+    this.readings = [];
+    for (let i = 0; i < this.rays.length; i++) {
+      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+    }
+  }
+
+  // return the closest touch {x,y,offset}
+  #getReading(ray, roadBorders) {
+    let touches = [];
+
+    // getIntersection returns an object with x, y, offset (meaning how far the obstable is from the center of the car)
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0], // ray start,
+        ray[1], // ray end
+        roadBorders[i][0], // road border top point
+        roadBorders[i][1] // road border bottom point
+      );
+      if (touch) {
+        touches.push(touch);
+      }
+    }
+
+    if (touches.length === 0) {
+      return null;
+    } else {
+      const offsets = touches.map((t) => t.offset);
+      // return the minimum offest aka the nearest one
+      const minOffset = Math.min(...offsets);
+      return touches.find((touch) => touch.offset === minOffset);
+    }
+  }
+
+  #castRays() {
+    this.rays = [];
+
+    for (let i = 0; i < this.rayCount; i++) {
+      const lerpT = this.rayCount === 1 ? 0.5 : i / (this.rayCount - 1); // minuse one here cause `i` never reaches rayCount
+      const rayAngle =
+        lerp(this.raySpread / 2, -this.raySpread / 2, lerpT) + this.car.angle;
+
+      const start = { x: this.car.x, y: this.car.y };
+      const end = {
+        x: this.car.x - Math.sin(rayAngle) * this.rayLength, // we can change the direction of rays if we use addition here
+        y: this.car.y - Math.cos(rayAngle) * this.rayLength,
+      };
+
+      this.rays.push([start, end]);
+    }
+  }
+
+  draw(ctx) {
+    for (let i = 0; i < this.rayCount; i++) {
+      // draw readings if there is a touch
+      let rayEndPoint = this.rays[i][1];
+      if (this.readings[i]) {
+        rayEndPoint = this.readings[i]; // update end point to the touch point
+      }
+
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+      ctx.lineTo(rayEndPoint.x, rayEndPoint.y);
+      ctx.stroke();
+
+      // see where the original line would be
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "black";
+      ctx.moveTo(rayEndPoint.x, rayEndPoint.y);
+      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.stroke();
+    }
+  }
+}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("myCanvas");
 const speedDiv = document.getElementById("speed");
 
-canvas.width = 200;
+canvas.width = 300;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
@@ -12,11 +12,11 @@ const grid = new Grid(canvas.width, window.innerHeight);
 animate();
 
 function animate() {
-  car.update();
+  car.update(road.borders);
 
   canvas.height = window.innerHeight; // resizing height & clear the canvas so that we can see car moving
 
-  // grid.draw(ctx);
+  //grid.draw(ctx);
 
   ctx.save();
   ctx.translate(0, -car.y + canvas.height * 0.75); // to create the illusion that car is moving forward
